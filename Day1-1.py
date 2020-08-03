49bb15dc63456282147c3f201628632d8439e747
miapin <69131907+miapin@users.noreply.github.com>
2020-08-03 11:14:04 +0800

    Add files via upload

---
+++ Day1-1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:05:39 2020
+
+@author: AE401
+"""
+
+print("I'm Eric")
+print('He is "cool"')
+print("I'm Eric\nHe is \"cool\"")
---
+++ Day1-2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:27:43 2020
+
+@author: AE401
+"""
+
+name=input("What's your name?  ")
+print("Hi,"+name+".")
